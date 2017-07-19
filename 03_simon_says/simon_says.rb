4aa3dbce28d2d3fbe4f8f9838555e27c631581f9
jttmckee <wp.jason@hotmail.co.uk>
2017-07-19 14:57:56 +0100

    Book project and fixed error with Simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -25,7 +25,7 @@ def titleize words
   wordsArray = words.split(" ")
   littleWords = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or",  "nor", "over"]
   wordsArray.each_index do |i|
-    if not (littleWords.include?(wordsArray[i])) or i == 0 then  wordsArray[i].capitalize end
+    if not (littleWords.include?(wordsArray[i])) or i == 0 then  wordsArray[i].capitalize! end
   end
   return wordsArray.join(" ")
 end
