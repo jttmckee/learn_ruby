a084a1851e0a99a6679d8a5f617ae177a56d647c
jttmckee <wp.jason@hotmail.co.uk>
2017-07-19 15:14:20 +0100

    Timer finished

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    timeTaken = @seconds
+    hours = (timeTaken / 3600).to_s.rjust(2, '0')
+    timeTaken = timeTaken - (timeTaken / 3600) * 3600
+    minutes = (timeTaken / 60).to_s.rjust(2, '0')
+    timeTaken = timeTaken - (timeTaken / 60) * 60
+    seconds = timeTaken.to_s.rjust(2, '0')
+
+    return "#{hours}:#{minutes}:#{seconds}"
+
+  end
+
+
 end
