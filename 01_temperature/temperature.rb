c3a35b47070601af19c98063c8c081d360eb42e2
jttmckee <wp.jason@hotmail.co.uk>
2017-07-19 13:44:57 +0100

    3 simon says completed

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+def ftoc temp
+  return (temp.to_f - 32) * 5/9
+
+end
+
+def ctof temp
+  return temp.to_f * 9/5 + 32
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
 #write your code here
+def echo words
+  return words
+end
+
+def shout words
+  return words.upcase
+end
+
+def repeat words, numTimes = 2
+  #return (words + " ")*(numTimes - 1) + words
+  #Above works, so does below.  Not sure which is more elegant?
+  return ((words + " ") * numTimes).chop
+end
+
+def start_of_word word, numLetters = 1
+  return word[0...numLetters]
+end
+
+def first_word words
+  return words.split(" ")[0]
+end
+
+def titleize words
+  wordsArray = words.split(" ")
+  littleWords = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or",  "nor", "over"]
+  wordsArray.each_index do |i|
+    if not (littleWords.include?(wordsArray[i])) or i == 0 then  wordsArray[i].capitalize end
+  end
+  return wordsArray.join(" ")
+end
